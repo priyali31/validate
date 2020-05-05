2b7836f47ed289c17f572c61a8b6abfaaaeb0777
priyali31 <60377704+priyali31@users.noreply.github.com>
2020-05-05 23:28:20 +0530

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -1,4 +1,43 @@
 #! /bin/bash
+   read -p "Enter username: " name
+    echo
+    if [[ $name =~ ^[?A-Z][a-z]{3,6}$ ]]
+    then
+        echo "name $name is valid."
+        break
+    else
+        echo "name $name is invalid."
+    fi
+    
+    read -p "Enter lastname: " name
+    echo
+    if [[ $name =~ ^[?A-Z][a-z]{3,6}$ ]]
+    then
+        echo "lastname $name is valid."
+        break
+    else
+        echo "lastname $name is invalid."
+    fi
+    
+    read -p "Enter email: " email
+    echo
+    if [[ $email =~ ^[a-zA-Z0-9.+]+@[a-z]+\.[a-z]{2,4}[.][a-z]{2}$ ]]
+    then
+        echo "email $email is valid."
+        break
+    else
+        echo "email $email is invalid."
+    fi
+    
+    read -p "Enter mobile number: " mobile
+    echo
+    if [[ $mobile =~ ^[a-zA-Z0-9.+]+@[a-z]+\.[a-z]{2,4}[.][a-z]{2}$ ]]
+    then
+        echo "mobile number $mobile is valid."
+        break
+    else
+        echo "mobile number $mobile is invalid."
+    fi
 
 read -p "Enter password" password
 
