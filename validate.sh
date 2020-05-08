93629d1df064247ee04c594fce001bddf5d6673d
priyali31 <60377704+priyali31@users.noreply.github.com>
2020-05-08 17:54:32 +0530

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -3,7 +3,7 @@ while true
 do
     read -p "Enter username: " fname
     echo
-    if [[ $name =~ ^[?A-Z][a-z]{3,6}$ ]]
+    if [[ $fname =~ ^[?A-Z][a-z]{3,6}$ ]]
     then
         echo "name $fname is valid."
     else
